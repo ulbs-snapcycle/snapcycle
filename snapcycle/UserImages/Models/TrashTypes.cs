2bf1d2cdccacda3eb3db4f3c409d24159802fbc7
Marius Achim <mariusachim2004@gmail.com>
2023-11-17 15:36:30 +0200

    added trash types

--- snapcycle/UserImages/Models/TrashTypes.cs
+++ snapcycle/UserImages/Models/TrashTypes.cs
@@ -1,6 +1,29 @@
 namespace snapcycle.UserImages.Models;
 
-public enum TrashTypes
+public enum TrashType
 {
-    
+    Paper,
+    Plastic,
+    Glass,
+    Metal,
+    Organic,
+    Electronic
+}
+
+public class TrashTypeMapping
+{
+    public static Dictionary<TrashType, string> trashTypeMap = new Dictionary<TrashType, string>()
+    {
+        { TrashType.Paper, "Paper" },
+        { TrashType.Plastic, "Plastic" },
+        { TrashType.Glass, "Glass" },
+        { TrashType.Metal, "Metal" },
+        { TrashType.Organic, "Organic" },
+        { TrashType.Electronic, "Electronic" },
+    };
+
+    public static string GetString(TrashType value)
+    {
+        return trashTypeMap.TryGetValue(value, out var result) ? result : throw new ArgumentException($"No mapping defined for {value}");
+    }
 }
