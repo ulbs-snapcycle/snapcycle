d608911ffe8aa58117b71000762823ef9d266092
flcristian <qflorescucristian@gmail.com>
2023-11-18 22:50:36 +0200

    finished project

--- html/script/player.js
+++ html/script/player.js
@@ -5,7 +5,7 @@ function getQueryParameter(name) {
 }
 
 const image = document.getElementById("image");
-const upload = document.getElementById("uploadImage");
+let upload = document.getElementById("uploadImage");
 const button = document.getElementById("uploadButton");
 const trash = document.getElementById('trash-list');
 const score = document.getElementById("score");
@@ -30,12 +30,49 @@ function updateScore(){
         .then(response => {return response.json()})
         .then(data => {
             console.log(data);
-            score.innerHTML = "Your Score : ${data.score}";
+            score.innerHTML = `Your Score : ${data.score}`;
         });
 }
 
 function uploadImage(){
+    const baseUrl = "http://localhost:5068/api/v1/";
+    const imagesUrl = baseUrl + "Images/";
+    const userImagesUrl = baseUrl + "UserImages/";
+    const fileInput = document.getElementById('uploadImage');
+    const file = fileInput.files[0];
 
+    if (file) {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        fetch(imagesUrl + "create", {
+            method: 'POST',
+            body: formData
+        })
+            .then(response => response.json())
+            .then(data => {
+                const headers = {
+                    userId: parseInt(userId),
+                    imageId: data.id,
+                    trashType: parseInt(trash.value)
+                }
+
+                fetch(userImagesUrl + "create", {
+                    method: 'POST',
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(headers)
+                })
+                    .then(response => {return response.json()})
+                    .then(data => {
+                        console.log(data);
+                    });
+            });
+    } else {
+        console.error('No file selected');
+    }
 }
 
 function buttonClick() {
